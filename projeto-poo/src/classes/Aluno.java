4299fa6d31a245b3d41fdc6a18755e9783713c2e
LucasDandary1 <dandarydrive@gmail.com>
2022-08-03 17:52:04 -0300

    CUMMIT DA AULA GETTER AND SETTER

--- projeto-poo/src/classes/Aluno.java
+++ projeto-poo/src/classes/Aluno.java
@@ -2,4 +2,69 @@ package classes;
 
 public class Aluno {
 
+	public String nome;
+
+	public String cpf;
+	public int idade;
+	public String dataNascimento;
+	public String nomeMae;
+	public String nomepai;
+	public String serieMatriculado;
+
+//===========================================//
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getNomepai() {
+		return nomepai;
+	}
+
+	public void setNomepai(String nomepai) {
+		this.nomepai = nomepai;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
 }
--- projeto-poo/src/codigos/exeutaveis/GeterSeter.java
+++ projeto-poo/src/codigos/exeutaveis/GeterSeter.java
@@ -1,5 +1,56 @@
 package codigos.exeutaveis;
 
+import classes.Aluno;
+
 public class GeterSeter {
+
+	public static void main(String[] args) {
+
+		// Aluno() == Instância, uma criação de um objt
+		// aluno 1 é uma referência para o objt Aluno
+		Aluno aluno1 = new Aluno();
+		aluno1.setNome("Lucas Dandary de Paula Ferreira");
+		aluno1.setCpf("022.274.716-11");
+		aluno1.setDataNascimento("23/09/2001");
+		aluno1.setIdade(20);
+		aluno1.setNomeMae("Teonélia Juvenila de Paula");
+		aluno1.setNomepai("Deivison Nelson Ferreira");
+		aluno1.setSerieMatriculado("3 ano do Ensino Médio");
+		//===========================================================//
+		System.out.println("Segue abaixo os dados dos seguntes alunos:");
+		System.out.println(" aluno1:");
+		System.out.println("  NOME = " + aluno1.getNome());
+		System.out.println("  CPF = " + aluno1.getCpf());
+		System.out.println("  DATA/NASCIMENTO = " + aluno1.getDataNascimento());
+		System.out.println("  IDADE = " + aluno1.getIdade());
+		System.out.println("  NOME DA MÃE = " + aluno1.getNomeMae());
+		System.out.println("  NOME DO PAI = " + aluno1.getNomepai());
+		System.out.println("  SERIE/MATRICULADO = " + aluno1.getSerieMatriculado());
+
+		System.out.println(""
+				+ "");
+		
+		//=======================================================================================================================================================================
+		
+		Aluno aluno2 = new Aluno();
+		aluno2.setNome("Lyla de Paula Avelar");
+		aluno2.setCpf("xxx.xxx.xxx-xx");
+		aluno2.setDataNascimento("16/06/2006");
+		aluno2.setIdade(18);
+		aluno2.setNomeMae("Teonélia Juvenila de Paula");
+		aluno2.setNomepai("Decio Avelar");
+		aluno2.setSerieMatriculado("3 ano do Ensino Médio");
+	
+		System.out.println(" aluno2:");
+		System.out.println("  NOME = " + aluno2.getNome());
+		System.out.println("  CPF = " + aluno2.getCpf());
+		System.out.println("  DATA/NASCIMENTO = " + aluno2.getDataNascimento());
+		System.out.println("  IDADE = " + aluno2.getIdade());
+		System.out.println("  NOME DA MÃE = " + aluno2.getNomeMae());
+		System.out.println("  NOME DO PAI = " + aluno2.getNomepai());
+		System.out.println("  SERIE/MATRICULADO = " + aluno2.getSerieMatriculado());
+		
+
+	}
 
 }
