4128d809e17ff53c0687b0cbc34fc7b48bc34827
LucasDandary1 <dandarydrive@gmail.com>
2022-08-08 21:08:08 -0300

    Criando projeto aluno

---
+++ ProjetoAluno/src/classe/Aluno.java
@@ -0,0 +1,133 @@
+package classe;
+
+public class Aluno {
+	
+	public String nome;
+
+	public String cpf;
+	public int idade;
+	public String dataNascimento;
+	public String nomeMae;
+	public String nomepai;
+	public String serieMatriculado;
+	public double nota1;
+	public double nota2;
+	public double nota3;
+	public double nota4;
+
+//===========================================//
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getNomepai() {
+		return nomepai;
+	}
+
+	public void setNomepai(String nomepai) {
+		this.nomepai = nomepai;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public double getNota1() {
+		return nota1;
+	}
+
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+
+	public double getNota2() {
+		return nota2;
+	}
+
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
+
+	public double getNota3() {
+		return nota3;
+	}
+
+	public void setNota3(double nota3) {
+		this.nota3 = nota3;
+	}
+
+	public double getNota4() {
+		return nota4;
+	}
+
+	public void setNota4(double nota4) {
+		this.nota4 = nota4;
+	}
+
+// Métdo que retorna a Média do Aluno.
+// Blocos que precisam de dados em que todos estão dentro do objeto
+// podem ser executados nele e retornar em outros lugares. 
+	public double getMediaNota() {
+		return (nota1 + nota2 + nota3 + nota4) / 4;
+	}
+
+	public boolean getAlunoAprovado() {
+		double media = this.getMediaNota();
+		if (media >= 70) {
+			return true;
+		}else {
+			return false;}
+			}
+	
+	public String getAlunoAprovado2() {
+		
+		double media = this.getMediaNota();
+			if (media >= 70) {
+				return "Aluno está aprovado";
+			}else {
+				return "Aluno está reprovado" ;
+			}
+		}
+		
+
+}
---
+++ ProjetoAluno/src/códigos/ClasseExecutora.java
@@ -0,0 +1,69 @@
+package códigos;
+
+import classe.Aluno;
+
+public class ClasseExecutora {
+	public static void main (String[] args) {
+
+
+		// Aluno() == Instância, uma criação de um objt
+		// aluno 1 é uma referência para o objt Aluno
+		Aluno aluno1 = new Aluno();
+		aluno1.setNome("Lucas Dandary de Paula Ferreira");
+		aluno1.setCpf("022.274.716-11");
+		aluno1.setDataNascimento("23/09/2001");
+		aluno1.setIdade(20);
+		aluno1.setNomeMae("Teonélia Juvenila de Paula");
+		aluno1.setNomepai("Deivison Nelson Ferreira");
+		aluno1.setSerieMatriculado("3 ano do Ensino Médio");
+		aluno1.setNota1(80);
+		aluno1.setNota2(59.4);
+		aluno1.setNota3(90.8);
+		aluno1.setNota4(90);
+
+		// ===========================================================//
+		System.out.println("Segue abaixo os dados dos seguntes alunos:");
+		System.out.println(" aluno1:");
+		System.out.println("  NOME ´3 igual a = " + aluno1.getNome());
+		System.out.println("  CPF = " + aluno1.getCpf());
+		System.out.println("  DATA/NASCIMENTO = " + aluno1.getDataNascimento());
+		System.out.println("  IDADE = " + aluno1.getIdade());
+		System.out.println("  NOME DA MÃE = " + aluno1.getNomeMae());
+		System.out.println("  NOME DO PAI = " + aluno1.getNomepai());
+		System.out.println("  SERIE/MATRICULADO = " + aluno1.getSerieMatriculado());
+
+		System.out.println("" + "");
+
+		// =======================================================================================================================================================================
+
+		Aluno aluno2 = new Aluno();
+		aluno2.setNome("Lyla de Paula Avelar");
+		aluno2.setCpf("xxx.xxx.xxx-xx");
+		aluno2.setDataNascimento("16/06/2006");
+		aluno2.setIdade(18);
+		aluno2.setNomeMae("Teonélia Juvenila de Paula");
+		aluno2.setNomepai("Decio Avelar");
+		aluno2.setSerieMatriculado("3 ano do Ensino Médio");
+		aluno2.setNota1(23.9);
+		aluno2.setNota2(23.5);
+		aluno2.setNota3(89.9);
+		aluno2.setNota4(67.8);
+
+		System.out.println(" aluno2:");
+		System.out.println("  NOME = " + aluno2.getNome());
+		System.out.println("  CPF = " + aluno2.getCpf());
+		System.out.println("  DATA/NASCIMENTO = " + aluno2.getDataNascimento());
+		System.out.println("  IDADE = " + aluno2.getIdade());
+		System.out.println("  NOME DA MÃE = " + aluno2.getNomeMae());
+		System.out.println("  NOME DO PAI = " + aluno2.getNomepai());
+		System.out.println("  SERIE/MATRICULADO = " + aluno2.getSerieMatriculado());
+
+		System.out.println("=================================================================== ");
+		System.out.println("A Média das notas dos alunos é:");
+		System.out.println("aluno 1:" + aluno1.getMediaNota());
+		System.out.println("aluno 2:" + aluno2.getMediaNota());
+		System.out.println("Resultado = " + (aluno1.getAlunoAprovado()? "Aprovado" : "Reprovado")) ;
+		System.out.println("Resultado 2 = " + aluno2.getAlunoAprovado2());
+		}
+
+}
